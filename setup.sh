241eb6634262ff7fb01ee4d1a7ce9a99643e4add
supermeap123 <garrentvanmeter@gmail.com>
2018-10-20 01:36:49 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ apt-get update
 apt-get install resilio-sync curl -y
 mkdir /home/rslsync/
 curl https://raw.githubusercontent.com/supermeap123/rslsync/master/config.conf > /home/rslsync/config.conf
-sudo su rslsync 
-rslsync --identity skeet --nodaemon
+mkdir /home/rslsync/sync/
 rslsync --config /home/rslsync/config.conf
