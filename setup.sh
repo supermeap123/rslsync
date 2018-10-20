f8fcdff3b704502a6a93b57fb95ba9fee6ce2fad
supermeap123 <garrentvanmeter@gmail.com>
2018-10-20 00:28:34 -0700

    Skeet

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install resilio-sync
+curl (config.conf) > config.conf
+rslsync --config config.conf
