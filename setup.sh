2eb9cc9097ba411d5a5d2aa66ac1058924c365f9
supermeap123 <garrentvanmeter@gmail.com>
2018-10-20 01:45:34 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | tee /etc/apt/sources.list.d/resilio-sync.list
 wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | apt-key add -
 apt-get update
-apt-get install resilio-sync curl -y
-mkdir /home/rslsync/
-curl https://raw.githubusercontent.com/supermeap123/rslsync/master/config.conf > /home/rslsync/config.conf
 mkdir /home/rslsync/sync/
-rslsync --config /home/rslsync/config.conf
+rslsync --webui.listen 0.0.0.0:8888
+chromium-browser http://127.0.0.1:8888
