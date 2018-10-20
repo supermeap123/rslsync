ca29a2c118e032c3ab53b3d0afee75bd417565ef
supermeap123 <garrentvanmeter@gmail.com>
2018-10-20 00:34:45 -0700

    update with config

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-fr
 wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install resilio-sync
-curl (config.conf) > config.conf
+curl https://raw.githubusercontent.com/supermeap123/rslsync/master/config.conf > config.conf
 rslsync --config config.conf
